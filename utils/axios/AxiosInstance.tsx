7beba1096fa3205dea3ca16280e6d0aabd224a4d
ssssksss <ssssksss@gmail.com>
2022-04-10 00:35:10 +0900

    nginx ssl test

--- utils/axios/AxiosInstance.tsx
+++ utils/axios/AxiosInstance.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 const AxiosInstance = axios.create({
   //baseURL: "http://localhost:8080",
-  baseURL: "http://146.56.117.239:8080",
+  //baseURL: "http://146.56.117.239:8080",
+  baseURL: "https://blog-server.ssssksss.xyz",
   //timeout: 1000,
   headers: {
     "Content-Type": "application/json",
