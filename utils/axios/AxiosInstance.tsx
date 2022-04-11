39f73418e28b5f3411ae76444b7c8b997e1f1665
ssssksss <ssssksss@gmail.com>
2022-04-11 15:50:42 +0900

    test https://blog-server.ssssksss.xyz

--- utils/axios/AxiosInstance.tsx
+++ utils/axios/AxiosInstance.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 const AxiosInstance = axios.create({
   //baseURL: "http://localhost:8080",
-  baseURL: "https://blog-server.ssssksss.xyz:8443",
-  //baseURL: "https://blog-server.ssssksss.xyz",
+  //baseURL: "https://blog-server.ssssksss.xyz:8443",
+  //baseURL: "http://blog-server.ssssksss.xyz",
+  baseURL: "http://146.56.117.239:8080",
   //timeout: 1000,
   headers: {
     "Content-Type": "application/json",
