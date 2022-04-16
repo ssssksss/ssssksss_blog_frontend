58fce0c2283c16b0b06c788476bf74cdb0b1051b
ssssksss <60907697+ssssksss@users.noreply.github.com>
2022-04-17 02:33:27 +0900

    Update AxiosInstance.tsx

--- utils/axios/AxiosInstance.tsx
+++ utils/axios/AxiosInstance.tsx
@@ -2,8 +2,8 @@ import axios from "axios";
 const AxiosInstance = axios.create({
   //baseURL: "http://localhost:8080",
   //baseURL: "https://blog-server.ssssksss.xyz:8443",
-  //baseURL: "http://blog-server.ssssksss.xyz",
-  baseURL: "http://146.56.117.239:8080",
+  baseURL: "https://blog-server.ssssksss.xyz",
+  //baseURL: "http://146.56.117.239:8080",
   //timeout: 1000,
   headers: {
     "Content-Type": "application/json",
