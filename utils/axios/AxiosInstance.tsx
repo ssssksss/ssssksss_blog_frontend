394173b75939945b89465ddf6c1c28c712e0236d
ssssksss <ssssksss@gmail.com>
2022-04-25 21:03:12 +0900

    axios url 변경

--- utils/axios/AxiosInstance.tsx
+++ utils/axios/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:8080",
-  //baseURL: "https://blog-server.ssssksss.xyz",
+  //baseURL: "http://localhost:8080",
+  baseURL: "https://blog-server.ssssksss.xyz",
   //timeout: 1000,
   headers: {
     "Content-Type": "application/json",
