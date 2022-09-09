829135e0770129e4644c7edc559ce21c45becc4d
ssssksss <ssssksss@gmail.com>
2022-09-10 00:17:21 +0900

    수정

--- utils/axios/AxiosInstance.tsx
+++ utils/axios/AxiosInstance.tsx
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const AxiosInstance = axios.create({
   // baseURL: "http://localhost:8080",
-  baseURL: "http://blog-server.ssssksss.xyz",
+  baseURL: "https://blog-server.ssssksss.xyz",
   //timeout: 1000,
   headers: {
     "Content-Type": "application/json",
