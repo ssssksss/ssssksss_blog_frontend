4e0f08c8bc11c6bfdeffd041feb155986b312ce6
SK\ssssk <ssssksss@naver.com>
2023-07-01 02:57:56 +0900

    [수정] 이전 블로그 컴포넌트가 잘못 연결되어 있어서 수정

--- pages/blog1/[firstCategory]/[secondCategory]/[post]/update.tsx
+++ pages/blog1/[firstCategory]/[secondCategory]/[post]/update.tsx
@@ -1,34 +1,11 @@
-// import React from "react";
-// import styled from "@emotion/styled";
-// import Layout1 from "src/components/layout/Layout1";
-// import dynamic from "next/dynamic";
-// import Loading1 from "@/components/common/loading/Loading1";
-// import theme from "@/styles/theme";
-
-// const DynamicComponent = dynamic(
-//   () => import("src/components/blog/Editor/CUEditor"),
-//   {
-//     ssr: false,
-//     loading: () => <Loading1 />,
-//   }
-// ) as any;
-
-// const Update = () => {
-//   return (
-//     <Container>
-//       <DynamicComponent edit={true} />
-//     </Container>
-//   );
-// };
-// Update.layout = Layout1;
-// export default Update;
-
-import BlogLayout from "@/components/layout/BlogLayout";
 import React from "react";
+import styled from "@emotion/styled";
+import BlogLayout from "@/components/layout/BlogLayout";
 import dynamic from "next/dynamic";
 import Loading1 from "@/components/common/loading/Loading1";
+import theme from "@/styles/theme";
 
-const DynamicComponent = dynamic(() => import("src/components/blog/BlogUI/BlogItemView"), {
+const DynamicComponent = dynamic(() => import("src/components/blog/Editor/CUEditor"), {
   ssr: false,
   loading: () => <Loading1 />,
 }) as any;
@@ -36,10 +13,9 @@ const DynamicComponent = dynamic(() => import("src/components/blog/BlogUI/BlogIt
 const Update = () => {
   return (
     <div>
-      <DynamicComponent />
+      <DynamicComponent edit={true} />
     </div>
   );
 };
-
-export default Update;
 Update.layout = BlogLayout;
+export default Update;
