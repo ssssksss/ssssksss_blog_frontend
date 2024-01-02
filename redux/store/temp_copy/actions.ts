ae8033720192171fca0d79422774331f250373e7
SK\ssssk <ssssksss@naver.com>
2024-01-03 02:28:58 +0900

    [fix] 복사용 코드 수정

--- redux/store/temp_copy/actions.ts
+++ redux/store/temp_copy/actions.ts
@@ -2,21 +2,12 @@ export type ACTION_INSTANCE =
   | ReturnType<typeof SET_COPY1>
   | ReturnType<typeof SET_COPY2>;
 
-type COPY1_STATE = {
-  temp1: string;
+type MEMO_LIST_STATE = {
+  memoList: [];
 };
-export const SET_COPY1 = (payload: COPY1_STATE) => {
+export const SET_MEMO_LIST = (payload: MEMO_LIST_STATE) => {
   return {
-    type: 'COPY1_ACTION',
-    payload: payload,
-  };
-};
-type COPY2_STATE = {
-  temp2: string;
-};
-export const SET_COPY2 = (payload: COPY2_STATE) => {
-  return {
-    type: 'COPY2_ACTION',
+    type: 'MEMO_LIST',
     payload: payload,
   };
 };
--- redux/store/temp_copy/reducers.ts
+++ redux/store/temp_copy/reducers.ts
@@ -1,17 +1,14 @@
 import * as actions from './actions';
 const initialState = {
-  temp1: 'temp1',
-  temp2: 'temp2',
+  todoList: [],
 };
-export const categoryReducer = (
+export const todoReducer = (
   state = initialState,
   action: actions.ACTION_INSTANCE
 ) => {
   switch (action.type) {
-    case 'COPY1_ACTION':
-      return { ...state, temp1: action.payload };
-    case 'COPY2_ACTION':
-      return { ...state, temp2: action.payload };
+    case 'TODO_LIST':
+      return { ...state, todoList: action.payload };
     default:
       return state;
   }
