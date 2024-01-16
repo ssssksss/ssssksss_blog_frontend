edc4b80a6d44ffa0d4844a6ed711543d4952648f
SK\ssssk <ssssksss@naver.com>
2024-01-16 22:18:21 +0900

    [feat] redux middleware 기능 추가

--- redux/store/index.ts
+++ redux/store/index.ts
@@ -1,3 +1,16 @@
-import { createStore } from "redux";
-import rootReducer from "@/redux/store/reducers";
-export const store = createStore(rootReducer);
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from '@/redux/store/reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const testMiddleware = (store: any) => dispatch => action => {
+  //   console.log('index.ts 파일 : ', store);
+  //   console.log('index.ts 파일 : ', store.getState());
+  //   console.log('index.ts 파일 : ', action);
+  return dispatch(action);
+};
+
+export const store = createStore(
+  rootReducer,
+  ['Use Redux'],
+  composeWithDevTools(applyMiddleware(testMiddleware))
+);
