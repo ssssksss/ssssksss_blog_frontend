c3324df588d88835403816aa7be5f50deef86c3b
SK\ssssk <ssssksss@naver.com>
2024-02-16 15:14:13 +0900

    [chore] sweetalert2 라이브러리 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,6 @@
+import { LoadingComponent } from '@/components/common/loading/LoadingComponent';
 import styled from '@emotion/styled';
 import Layout1 from 'src/components/layout/Layout1';
-import { CC } from '@/styles/commonComponentStyle';
-import BasicCarousel from '@/components/common/carousel/BasicCarousel';
-import { commonTheme } from '@/styles/theme';
-import Stack from '@/components/intro/stack';
-import BlogFooter from '@/components/blog/BlogUI/BlogFooter';
-import React from 'react';
-import HomeThreejs from '@/components/Introduce/threejs/HomeThreejs';
-import Content from '@/components/threejs/Content';
-import { LoadingComponent } from '@/components/common/loading/LoadingComponent';
 
 const HomePage = ({ test }: any) => {
   return (
