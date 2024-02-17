710e3d2a3ea19826fe200baeb179479b566d74e6
SK\ssssk <ssssksss@naver.com>
2024-02-17 11:15:58 +0900

    [minor fix] 주석 제거

--- next.config.js
+++ next.config.js
@@ -22,17 +22,5 @@ const nextConfig = withPWA({
     appDir: true,
   },
 });
-// const nextConfig = {
-//   reactStrictMode: false,
-//   typescript: {
-//     ignoreBuildErrors: true,
-//   },
-//   compiler: {
-//     emotion: true,
-//   },
-//   images: {
-//     domains: ['ssssksssblogbucket.s3.ap-northeast-2.amazonaws.com'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
-//   },
-// };
 
 module.exports = nextConfig;
