688ed8025c2530d7ebf695177980a6cda0fc72f0
SK\ssssk <ssssksss@naver.com>
2024-02-22 01:51:53 +0900

    [minor fix] 링크 연결

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -41,16 +41,45 @@ const Footer = () => {
     <Container>
       <ImageList gap={8}>
         <li>
-          <Image src={Icons.NotionIcon} width={'100%'} height={'100%'} />
+          <a
+            href={'https://www.notion.so/1e30249dcdf741398637235d6d251552'}
+            target={'_blanket'}
+          >
+            <Image src={Icons.NotionIcon} width={'100%'} height={'100%'} />
+          </a>
         </li>
         <li>
-          <Image src={Icons.ErdCloudIcon} width={'100%'} height={'100%'} />
+          <a
+            href={'https://www.erdcloud.com/d/GsKo8HnYFXHbrqJoQ'}
+            target={'_blanket'}
+          >
+            <Image src={Icons.ErdCloudIcon} width={'100%'} height={'100%'} />
+          </a>
         </li>
         <li>
-          <Image src={Icons.FigmaIcon} width={'100%'} height={'100%'} />
+          <a
+            href={
+              'https://www.figma.com/file/9NC19XbZokgmjBqk7TOQFg/%EC%BD%94%EB%94%A9%EC%9D%98-%EB%AA%A8%EB%93%A0-%EA%B2%83%EC%9D%84-%EC%A0%95%EB%A6%AC%ED%95%98%EB%8A%94-%EA%B3%B5%EA%B0%84?type=design&node-id=974%3A948&mode=design&t=Ydosap26M3YvcoFi-1'
+            }
+            target={'_blanket'}
+          >
+            <Image src={Icons.FigmaIcon} width={'100%'} height={'100%'} />
+          </a>
         </li>
         <li>
-          <Image src={Icons.GithubIcon} width={'100%'} height={'100%'} />
+          <a href={'https://github.com/ssssksss'} target={'_blanket'}>
+            <Image src={Icons.GithubIcon} width={'100%'} height={'100%'} />
+          </a>
+        </li>
+        <li>
+          <a
+            href={
+              'https://64eeece7369a220b140a6b70-gmqpbabxdz.chromatic.com/?path=/story/buttons--active'
+            }
+            target={'_blanket'}
+          >
+            <Image src={Icons.StoryBookIcon} width={'100%'} height={'100%'} />
+          </a>
         </li>
       </ImageList>
       <CC.ColumnDiv color={'contrast'} gap={16} pd={'8px 0px'}>
