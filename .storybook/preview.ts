8072533b66c0a830f00ed2450bcc3f60cf8676dc
SK\ssssk <ssssksss@naver.com>
2024-02-22 22:42:51 +0900

    [minor fix]

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,6 @@
 import type { Preview } from '@storybook/react';
 
 import { GlobalStyles } from '@/styles/GlobalStyles';
-import { darkTheme, purpleTheme } from '@/styles/theme';
-import { ThemeProvider } from '@emotion/react';
 import { withThemeFromJSXProvider } from '@storybook/addon-themes';
 
 const preview: Preview = {
@@ -18,12 +16,12 @@ const preview: Preview = {
 
   decorators: [
     withThemeFromJSXProvider({
-      themes: {
-        darkTheme: darkTheme,
-        purpleTheme: purpleTheme,
-      },
-      defaultTheme: 'purpleTheme',
-      Provider: ThemeProvider,
+      // themes: {
+      //   darkTheme: darkTheme,
+      //   purpleTheme: purpleTheme,
+      // },
+      // defaultTheme: 'purpleTheme',
+      // Provider: ThemeProvider,
       GlobalStyles,
     }),
   ],
