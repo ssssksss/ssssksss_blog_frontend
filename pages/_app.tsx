1613b7894e5bb18769003cfbe7ae645061681b03
SK\ssssk <ssssksss@naver.com>
2024-02-23 00:36:53 +0900

    [minor fix]

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,18 @@
-import { store } from '@/redux/store/index';
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
-import 'prismjs/themes/prism-tomorrow.css';
-import { ReactElement, ReactNode, StrictMode } from 'react';
-import 'react-quill/dist/quill.snow.css';
-import { Provider } from 'react-redux';
-// react-date-range 라이브러리
 import NavBar from '@/components/layout/NavBar';
+import { store } from '@/redux/store/index';
 import GlobalStyles from '@/styles/GlobalStyles';
 import { Global } from '@emotion/react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import 'prismjs/themes/prism-tomorrow.css';
+import { ReactElement, ReactNode, StrictMode } from 'react';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { QueryClient, QueryClientProvider } from 'react-query';
+import 'react-quill/dist/quill.snow.css';
+import { Provider } from 'react-redux';
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
