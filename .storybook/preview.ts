07641fad71f6089defe45d5bf3edb5f414d69c62
SK\ssssk <ssssksss@naver.com>
2024-02-23 23:52:21 +0900

    [prod] 스토리북 자동 배포 테스트

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -15,7 +15,35 @@ const preview: Preview = {
       },
     },
   },
-
+  viewport: {
+    viewports: {
+      mobile1: {
+        name: 'iPhone 13 mini',
+        styles: {
+          width: '375px',
+          height: '812px',
+        },
+        type: 'mobile',
+      },
+      mobile2: {
+        name: 'iPhone 13 / 13 pro',
+        styles: {
+          width: '390px',
+          height: '844px',
+        },
+        type: 'mobile',
+      },
+      tablet1: {
+        name: 'iPad Pro 11"',
+        styles: {
+          width: '834px',
+          height: '1194px',
+        },
+        type: 'tablet',
+      },
+    },
+    defaultViewport: 'mobile1',
+  },
   decorators: [
     withThemeFromJSXProvider({
       themes: {
