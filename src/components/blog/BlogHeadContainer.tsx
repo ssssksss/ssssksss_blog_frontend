c230cf56e9abc659d5fd874b7ba2608676fdaf20
SK\ssssk <ssssksss@naver.com>
2024-03-02 20:26:44 +0900

    [update] 검색 무한스크롤로 변경

--- src/components/blog/BlogHeadContainer.tsx
+++ src/components/blog/BlogHeadContainer.tsx
@@ -1,11 +1,14 @@
 import { BlogAPI } from '@api/BlogAPI';
 import { Icons } from '@components/common/icons/Icons';
 import Input from '@components/common/input/Input';
+import useIntersection from '@components/useHook/useIntersection';
 import styled from '@emotion/styled';
+import { store } from '@redux/store';
 import { CC } from '@styles/commonComponentStyle';
 import { delaySearch } from '@utils/function/delaySearch';
 import Link from 'next/link';
 import { useEffect, useRef, useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
 import BlogItem from './BlogItem';
 /**
  * @author Sukyung Lee <ssssksss@naver.com>
@@ -15,24 +18,65 @@ import BlogItem from './BlogItem';
  */
 const BlogHeadContainer = () => {
   const [isOpenBlogItemList, setIsOpenBlogItemList] = useState(false);
-  const [searchBlogList, setSearchBlogList] = useState([]);
+  const [isInputChange, setIsInputChange] = useState(false);
   const inputRef = useRef<null>();
 
+  const {
+    data: blogListResData,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+  } = useInfiniteQuery(
+    ['key'],
+    ({ pageParam = 1 }) =>
+      BlogAPI.getSearchBlogList(inputRef.current.value, pageParam),
+    {
+      refetchOnWindowFocus: false,
+      select: (data) => {
+        let temp = [];
+        data?.pages.map((i) => {
+          let temp1 = [
+            ...i.json.blogList.map((_i) => {
+              return {
+                ..._i,
+                defaultImageUrl: store
+                  .getState()
+                  .blogStore.blogCategoryList.filter(
+                    (j) => j.id == _i.firstCategoryId,
+                  )[0]
+                  ?.secondCategoryList.filter(
+                    (k) => k.id == _i.secondCategoryId,
+                  )[0].thumbnailImageUrl,
+              };
+            }),
+          ];
+          temp = temp.concat(temp1);
+        });
+        return temp;
+      },
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length + 1;
+        return lastPage?.json.blogList?.length < 10 ? undefined : nextPage;
+      },
+      enabled: isOpenBlogItemList && isInputChange,
+    },
+  );
+
+  const infiniteScrollRef = useIntersection((entry, observer) => {
+    observer.unobserve(entry.target);
+    if (hasNextPage && !isFetching) fetchNextPage();
+  });
+
   const SearchHandler = () => {
-    BlogAPI.getSearchBlogList({
-      keyword: inputRef.current.value,
-    }).then((res) => {
-      // ! API쪽 코드 응답 형식이 달라서 나중에 수정 필요
-      setSearchBlogList(res?.data?.blogList);
-      res.data?.blogList?.length > 0
-        ? setIsOpenBlogItemList(true)
-        : setIsOpenBlogItemList(false);
-    });
+    setIsOpenBlogItemList(true);
+    setIsInputChange(true);
   };
 
   useEffect(() => {
     const temp = () => {
+      // 검색시 리스트를 보여주는 화면과 입력할 경우에만 데이터
       setIsOpenBlogItemList(false);
+      setIsInputChange(false);
     };
     if (isOpenBlogItemList) {
       window.addEventListener('click', temp);
@@ -54,13 +98,8 @@ const BlogHeadContainer = () => {
         ref={inputRef}
         leftIconImage={Icons.SearchIcon.src}
         onChange={delaySearch(SearchHandler, 600)}
-        onClick={() =>
-          setIsOpenBlogItemList((prev) =>
-            searchBlogList?.length > 0 ? !prev : false,
-          )
-        }
+        onClick={() => setIsOpenBlogItemList((prev) => !prev)}
       />
-
       {isOpenBlogItemList && (
         <BlogSearchItemContainer
           onBlur={() => {
@@ -68,15 +107,21 @@ const BlogHeadContainer = () => {
           }}
           isEmpty={isOpenBlogItemList.length >= 1}
         >
-          {searchBlogList?.map((i, index) => (
+          {blogListResData?.map((j, index) => (
             <li key={index}>
-              <Link href={`/blog/${i.id}`} key={`${i.id}${index}`}>
+              <Link href={`/blog/${j.id}`} key={`${j.id}${index}`}>
                 <a>
-                  <BlogItem element={i}></BlogItem>
+                  <BlogItem element={j}></BlogItem>
                 </a>
               </Link>
             </li>
           ))}
+          {hasNextPage || (
+            <CC.RowCenterDiv minH={'30px'} bg={'primary40'} brR={'8px'}>
+              마지막 결과 입니다.
+            </CC.RowCenterDiv>
+          )}
+          <div ref={infiniteScrollRef}></div>
         </BlogSearchItemContainer>
       )}
     </Container>
@@ -99,17 +144,14 @@ const BlogSearchItemContainer = styled(CC.ColumnDiv.withComponent('ul'))`
   background: ${(props) => props.theme.main.contrast};
   outline: solid 4px ${(props) => props.theme.main.secondary80};
   padding: 16px;
-  {
-    props.isEmpty && 
-    outline: inset ${(props) => props.theme.main.primary80} 4px;
-  }
   transform: translate(-4px, 6px);
   border-radius: 10px;
   gap: 8px;
   z-index: 40;
+  overflow: scroll;
+  height: calc(100vh - 104px);
   @media (max-height: 624px) {
     ${(props) => props.theme.scroll.hidden};
-    height: calc(100vh - 104px);
   }
 
   a {
