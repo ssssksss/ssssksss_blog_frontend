df2e73aa8151e09339a37d93d62fb757698f9dea
SK\ssssk <ssssksss@naver.com>
2024-03-04 16:58:28 +0900

    [minor fix]

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
+import TagList from '@components/common/tag/TagList';
 import Footer from '@components/layout/Footer';
+import MyRoom from '@components/threejs/glTF/room/MyRoom';
 import styled from '@emotion/styled';
+import { CC } from '@styles/commonComponentStyle';
 import Head from 'next/head';
 import Layout1 from 'src/components/layout/Layout1';
 
@@ -9,13 +12,26 @@ const HomePage = () => {
       <Head>
         <title> 홈 화면 </title>
       </Head>
-      {/* <HomeThreejs />
-      <Content /> */}
-      <div> 홈 화면 아직 미구성 </div>
-      <div>
-        블로그는 실행했던 코드나 정리를 위한 블로그, 아직 설명용도의 블로그는
-        아닙니다.
-      </div>
+      <CC.RowCenterDiv>
+        <h2> 홈 화면 아직 미구성, 다른 개발 공부 중 </h2>
+      </CC.RowCenterDiv>
+      <TagList
+        bg={'secondary40'}
+        data={[
+          'SEO && 코드 스플리팅 & Lazy Loading & 최적화',
+          '타입스크립트',
+          '서비스 api 로직 분리',
+          '커스텀 컴포넌트들 제작',
+          '블로그 정리',
+          '백엔드 response 수정',
+          '백엔드 코드 정리',
+          'FE,BE 테스트 코드 공부',
+          'BE Jenkins 재시도',
+        ]}
+      />
+      <ThreeJSBox>
+        <MyRoom />
+      </ThreeJSBox>
       <Footer />
     </Container>
   );
@@ -25,6 +41,11 @@ export default HomePage;
 
 const Container = styled.main`
   width: 100%;
-  height: 100%;
   position: relative;
+`;
+
+const ThreeJSBox = styled(CC.RowCenterDiv)`
+  width: calc(100% - 60px);
+  height: 300px;
+  padding-left: 30px;
 `;
