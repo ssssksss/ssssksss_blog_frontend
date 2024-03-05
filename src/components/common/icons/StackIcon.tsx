ccde15ba9369e3e6f318e6e2083537d0d27f5ae9
SK\ssssk <ssssksss@naver.com>
2024-03-05 16:17:39 +0900

    [add] stack소개에 필요한 이미지들

---
+++ src/components/common/icons/StackIcon.tsx
@@ -0,0 +1,45 @@
+import AwsS3 from '/public/img/stack-icon/aws-s3.svg';
+import Css from '/public/img/stack-icon/css.svg';
+import Emotion from '/public/img/stack-icon/emotion.svg';
+import Figma from '/public/img/stack-icon/figma.svg';
+import Html from '/public/img/stack-icon/html5.svg';
+import Javascript from '/public/img/stack-icon/javascript.svg';
+import Jenkins from '/public/img/stack-icon/jenkins.svg';
+import Linux from '/public/img/stack-icon/linux.svg';
+import Mysql from '/public/img/stack-icon/mysql.svg';
+import Nextjs from '/public/img/stack-icon/nextjs.svg';
+import Nginx from '/public/img/stack-icon/nginx.svg';
+import ReactQuery from '/public/img/stack-icon/react-query.svg';
+import React from '/public/img/stack-icon/reactjs.svg';
+import Redux from '/public/img/stack-icon/redux.svg';
+import SpringBoot from '/public/img/stack-icon/spring-boot.svg';
+import SpringDataJpa from '/public/img/stack-icon/spring-data-jpa.svg';
+import SpringSecurity from '/public/img/stack-icon/spring-security.svg';
+import Storybook from '/public/img/stack-icon/storybook.svg';
+import Swagger from '/public/img/stack-icon/swagger.svg';
+import Tomcat from '/public/img/stack-icon/tomcat.svg';
+import Typescript from '/public/img/stack-icon/typescript.svg';
+
+export const StackIcon = {
+  Html,
+  Css,
+  Javascript,
+  Typescript,
+  React,
+  Nextjs,
+  Redux,
+  Emotion,
+  ReactQuery,
+  Storybook,
+  SpringBoot,
+  SpringSecurity,
+  SpringDataJpa,
+  Swagger,
+  Mysql,
+  Figma,
+  Nginx,
+  Tomcat,
+  Linux,
+  AwsS3,
+  Jenkins,
+};
