5a3ac126ceaf10cf500c6484080d056fc065ab5d
SK\ssssk <ssssksss@naver.com>
2024-03-06 00:54:38 +0900

    [update] head 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,10 +7,13 @@ export default class MyDocument extends Document {
         <Head>
           <link rel="manifest" href="/manifest/manifest.json" />
           <meta name="theme-color" content="#84A59D" />
+          <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
           <meta
             name="description"
             content="가출한토토로의 개발자 사이트, 블로그, 게시판 일정 등등 여러 개인 프로젝트가 담긴 공간"
           />
+
+          {/* SEO 등록 */}
           <meta
             name="naver-site-verification"
             content="c25720a40b2a2d2e2ee2b0da57e9b5de26cd550e"
