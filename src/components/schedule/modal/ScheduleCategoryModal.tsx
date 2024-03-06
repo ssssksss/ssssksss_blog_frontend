cb46bbe6c6a2c44b3a8da02c1a17e038cbea9296
SK\ssssk <ssssksss@naver.com>
2024-03-07 01:55:27 +0900

    [fix] 이벤트 버블링 방지 코드 추가

--- src/components/schedule/modal/ScheduleCategoryModal.tsx
+++ src/components/schedule/modal/ScheduleCategoryModal.tsx
@@ -9,9 +9,9 @@ import UpdateScheduleCategoryBox from '../UpdateScheduleCategoryBox';
  * @version 0.0.1 "2023-12-13 15:34:42"
  * @description 설명
  */
-const ScheduleCategoryModal = props => {
+const ScheduleCategoryModal = (props) => {
   return (
-    <Container>
+    <Container onClick={(e) => e.stopPropagation()}>
       <AddScheduleCategoryBox closeModal={props.closeModal} />
       <UpdateScheduleCategoryBox closeModal={props.closeModal} />
       <DeleteScheduleCategoryBox closeModal={props.closeModal} />
@@ -23,10 +23,10 @@ export default ScheduleCategoryModal;
 const Container = styled(CC.ColumnDiv)`
   gap: 32px;
   padding: 0px 4px;
-  color: ${props => props.theme.colors.black80};
+  color: ${(props) => props.theme.colors.black80};
   overflow: scroll;
-  background: ${props => props.theme.main.primary40};
-  font-family: ${props => props.theme.fontFamily.cookieRunRegular};
-  font-size: ${props => props.theme.fontSize.xl};
+  background: ${(props) => props.theme.main.primary40};
+  font-family: ${(props) => props.theme.fontFamily.cookieRunRegular};
+  font-size: ${(props) => props.theme.fontSize.xl};
   min-height: 260px;
 `;
