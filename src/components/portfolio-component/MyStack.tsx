8332339d8e14adfb396db89b344f339570f6eb56
SK\ssssk <ssssksss@naver.com>
2024-03-08 03:21:58 +0900

    [update] 포트폴리오 수정 중

--- src/components/portfolio-component/MyStack.tsx
+++ src/components/portfolio-component/MyStack.tsx
@@ -20,38 +20,14 @@ interface IMyStackProps {
 const DevelopList = {
   Frontend: [
     {
-      stack: 'html',
-      img: StackIcon.Html,
-      desc: ['1', '2'],
-    },
-    {
-      stack: 'css',
-      img: StackIcon.Css,
-      desc: [
-        {
-          title: '배치',
-          desc: 'flex,grid,position 등을 사용하여 적절한 배치 가능',
-        },
-        {
-          title: '반응형',
-          desc: '화면 크기에 맞게 반응형 웹을 만들 수 있다',
-        },
-      ],
-    },
-    {
-      stack: 'javascript',
-      img: StackIcon.Javascript,
-      desc: ['3', '4'],
-    },
-    {
-      stack: 'typescript',
-      img: StackIcon.Typescript,
-      desc: ['3', '4'],
-    },
-    {
       stack: 'react',
       img: StackIcon.React,
-      desc: ['3', '4'],
+      desc: [
+        {
+          title: 'useHooks와 React.memo에 대한 이해 및 사용',
+          desc: 'useEffect, useRef, useRewardRef, useState, useReducer, useCallback, useMemo, React.memo',
+        },
+      ],
     },
     {
       stack: 'nextjs',
@@ -72,6 +48,57 @@ const DevelopList = {
       stack: 'storybook',
       img: StackIcon.Storybook,
       desc: ['1', '2'],
+    },
+    {
+      stack: 'typescript',
+      img: StackIcon.Typescript,
+      desc: ['3', '4'],
+    },
+    {
+      stack: 'html',
+      img: StackIcon.Html,
+      desc: [
+        {
+          title: '다양한 태그 사용법 습득',
+          desc: 'a, h, input, div, span, ul, select, details 등',
+        },
+        {
+          title: '시멘틱 태그',
+          desc: '아직 완벽하지는 않지만 시멘틱 태그 사용',
+        },
+      ],
+    },
+    {
+      stack: 'css',
+      img: StackIcon.Css,
+      desc: [
+        {
+          title: '배치',
+          desc: 'flex, grid, position 등을 사용하여 적절한 배치 가능',
+        },
+        {
+          title: '반응형',
+          desc: '화면에 맞게 반응형 웹을 만들 수 있다',
+        },
+      ],
+    },
+    {
+      stack: 'javascript',
+      img: StackIcon.Javascript,
+      desc: [
+        {
+          title: '스크롤, 라우터, setInterval, setTimeout',
+          desc: '스크롤 좌표 이해, 라우터 처리에 push나 replace에 대한 이해, 일정 시간 뒤 이벤트 작동',
+        },
+        {
+          title: '문자열, 배열, 객체 사용방법',
+          desc: '문자열, 배열, 객체와 관련된 메소드 사용방법',
+        },
+        {
+          title: '스토리지',
+          desc: '스토리지를 사용하여 최근목록이나 뮤직플레이어 url을 저장해놓는 작업',
+        },
+      ],
     },
   ],
   Backend: [
@@ -240,7 +267,7 @@ const DescriptionList = styled(CC.ColumnDiv.withComponent('ul'))`
 
   li {
     display: grid;
-    grid-template-columns: 120px auto;
+    grid-template-columns: 200px auto;
     div:nth-of-type(1) {
       font-weight: 600;
     }
