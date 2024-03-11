e6d420c0e7df5dbb0c16171ee585bdb815ce6018
SK\ssssk <ssssksss@naver.com>
2024-03-11 15:21:47 +0900

    [minor fix] 포트폴리오 수정

--- pages/portfolio/index.tsx
+++ pages/portfolio/index.tsx
@@ -1,10 +1,12 @@
 import TabMenu from '@components/common/tab/TabMenu';
 import Footer from '@components/layout/Footer';
-import Layout1 from '@components/layout/Layout1';
+import Layout2 from '@components/layout/Layout2';
 import MyStack from '@components/portfolio-component/MyStack';
 import Portfolio2024 from '@components/portfolio-component/Portfolio2024';
 import styled from '@emotion/styled';
 import { CC } from '@styles/commonComponentStyle';
+import { AWSS3Prefix } from '@utils/variables/url';
+import Image from 'next/image';
 /**
  * @author Sukyung Lee <ssssksss@naver.com>
  * @file index.tsx
@@ -31,55 +33,66 @@ const Index = () => {
     <Container>
       <Section>
         <CC.GridColumn2 gap={8}>
-          <CC.ColumnCenterCenterDiv
-            minH={'240px'}
-            gap={8}
-            outline={true}
-            pd={'2px'}
-          >
-            <CC.RowCenterDiv w={'160px'} h={'160px'} bg={'contrast'} pd={'4px'}>
-              이미지
-            </CC.RowCenterDiv>
-            <div>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the
-            </div>
+          <CC.ColumnCenterCenterDiv minH={'240px'} gap={8} pd={'2px'}>
+            <ImageBox w={'180px'} h={'240px'} bg={'transparent'} pd={'4px'}>
+              <Image
+                src={`${AWSS3Prefix}private/%EA%B0%9C%EC%9D%B8_%ED%94%84%EB%A1%9C%ED%95%84_%EC%82%AC%EC%A7%84_%EB%B0%B0%EA%B2%BD%EC%A0%9C%EA%B1%B0.png`}
+                layout="fill"
+              />
+              {/* <ReactPlayer
+                className="react-player"
+                url={`${AWSS3Prefix}private/%EC%9B%90%EB%B3%B8+_+202403100358+_+%EA%B0%9C%EC%9D%B8_%ED%94%84%EB%A1%9C%ED%95%84_%EC%82%AC%EC%A7%84_%EB%B0%B0%EA%B2%BD%EC%A0%9C%EA%B1%B0_animation.mp4`} // 영상 url
+                width="100%"
+                height="100%"
+                playing={true} // 자동 재생 on
+                muted={true} // 음소거 on
+                controls={false} // 플레이어 컨트롤 노출 여부
+                light={false} // 플레이어 모드
+                pip={false} // pip 모드 설정 여부
+                loop={false} // 반복재생
+              /> */}
+            </ImageBox>
           </CC.ColumnCenterCenterDiv>
-          <CC.GridRow4 h={'240px'} gap={16} outline={true} pd={'4px'}>
-            <CC.RowDiv bg={'contrast'} pd={'0px 0px 0px 4px'}>
+          <CC.GridRow4
+            h={'240px'}
+            gap={16}
+            pd={'4px'}
+            bg={'transparent'}
+            fontFamily={'gmarketSansBold'}
+            color={'white20'}
+          >
+            <CC.RowDiv pd={'0px 0px 0px 4px'} color={'white'}>
               이름 : 이수경
             </CC.RowDiv>
-            <CC.RowDiv bg={'contrast'} pd={'0px 0px 0px 4px'}>
+            <CC.RowDiv pd={'0px 0px 0px 4px'} color={'white'}>
               번호 : 010-7430-9809
             </CC.RowDiv>
-            <CC.RowDiv bg={'contrast'} pd={'0px 0px 0px 4px'}>
+            <CC.RowDiv pd={'0px 0px 0px 4px'} color={'white'}>
               메일 : ssssksss@naver.com
             </CC.RowDiv>
-            <CC.RowDiv bg={'contrast'} pd={'0px 0px 0px 4px'}>
-              링크 : 깃허브, 블로그
-            </CC.RowDiv>
+            <CC.RowDiv pd={'0px 0px 0px 4px'}>링크 : 깃허브, 블로그</CC.RowDiv>
           </CC.GridRow4>
         </CC.GridColumn2>
         <CC.GridColumn4 gap={8}>
-          <CC.ColumnCenterCenterDiv h={'120px'} gap={8} outline={true}>
+          <CC.ColumnCenterCenterDiv h={'120px'} gap={8}>
             <CC.RowCenterDiv w={'80px'} h={'80px'} bg={'contrast'}>
               이미지
             </CC.RowCenterDiv>
             <div> 설명 </div>
           </CC.ColumnCenterCenterDiv>
-          <CC.ColumnCenterCenterDiv h={'120px'} gap={8} outline={true}>
+          <CC.ColumnCenterCenterDiv h={'120px'} gap={8}>
             <CC.RowCenterDiv w={'80px'} h={'80px'} bg={'contrast'}>
               이미지
             </CC.RowCenterDiv>
             <div> 설명 </div>
           </CC.ColumnCenterCenterDiv>
-          <CC.ColumnCenterCenterDiv h={'120px'} gap={8} outline={true}>
+          <CC.ColumnCenterCenterDiv h={'120px'} gap={8}>
             <CC.RowCenterDiv w={'80px'} h={'80px'} bg={'contrast'}>
               이미지
             </CC.RowCenterDiv>
             <div> 설명 </div>
           </CC.ColumnCenterCenterDiv>
-          <CC.ColumnCenterCenterDiv h={'120px'} gap={8} outline={true}>
+          <CC.ColumnCenterCenterDiv h={'120px'} gap={8}>
             <CC.RowCenterDiv w={'80px'} h={'80px'} bg={'contrast'}>
               이미지
             </CC.RowCenterDiv>
@@ -89,6 +102,7 @@ const Index = () => {
       </Section>
       <Section>
         <TabMenu
+          bg={'transparent'}
           menu={[
             {
               tabName: '2024(1)',
@@ -106,18 +120,24 @@ const Index = () => {
     </Container>
   );
 };
-Index.layout = Layout1;
+Index.layout = Layout2;
 export default Index;
 
 const Container = styled(CC.ColumnDiv)`
   width: 100%;
   gap: 4px;
-  overflow-y: scroll;
+  ${(props) => props.theme.scroll.hidden};
+`;
+
+const ImageBox = styled(CC.RowCenterDiv)`
+  position: relative;
+  & > * {
+    background: transparent;
+  }
 `;
 
 const Section = styled.section`
   ${(props) => props.theme.flex.column};
-  background: ${(props) => props.theme.main.primary40};
   padding: 8px;
   gap: 8px;
 `;
