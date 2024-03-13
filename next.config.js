30d21d6ae2088778f925d9c03acb2cc129ffb2b8
SK\ssssk <ssssksss@naver.com>
2024-03-13 21:19:50 +0900

    [change] react 18 버전과 mdx editor를 사용하기 위한 코드 추가

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const removeImports = require('next-remove-imports')();
+
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
@@ -18,9 +20,16 @@ const nextConfig = withPWA({
   images: {
     domains: ['ssssksssblogbucket.s3.ap-northeast-2.amazonaws.com'], // 이곳에 에러에서 hostname 다음 따옴표에 오는 링크를 적으면 된다.
   },
-  experimental: {
-    appDir: true,
+  transpilePackages: ['@mdxeditor/editor'],
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true
+    return config;
   },
 });
 
-module.exports = nextConfig;
+module.exports = removeImports({
+  nextConfig,
+});
