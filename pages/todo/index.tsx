b547830c5e39453a76a3f7b9795e2b925ba54398
SK\ssssk <ssssksss@naver.com>
2024-03-14 10:06:32 +0900

    [fix] app.tsx에서 제거한 css 추가

--- pages/todo/index.tsx
+++ pages/todo/index.tsx
@@ -9,7 +9,10 @@ import Layout1 from '@components/layout/Layout1';
 import styled from '@emotion/styled';
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css';
 import { useSelector } from 'react-redux';
+
 /**
  * @author Sukyung Lee <ssssksss@naver.com>
  * @file index.tsx
