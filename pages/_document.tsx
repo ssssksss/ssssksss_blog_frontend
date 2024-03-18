6f6711ff3ef66829f7382052d3c416d39325b9e2
SK\ssssk <ssssksss@naver.com>
2024-03-18 17:38:30 +0900

    [seo] seo robots.txt와 meta 태그 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
       <Html lang="ko">
         <Head>
           <link rel="manifest" href="/manifest/manifest.json" />
-          <meta name="theme-color" content="#84A59D" />
+          <link rel="shortcut icon" href="/img/totoro.svg" />
           <meta httpEquiv="content-type" content="text/html; charset=UTF-8" />
           <meta
             name="description"
@@ -22,6 +22,37 @@ export default class MyDocument extends Document {
             name="google-site-verification"
             content="tbLLC2GI4ElFxdiITrMo0FmL5ZAnNBAB1x_EAxeLeJM"
           />
+          {/* op-graph 설정*/}
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://blog.ssssksss.xyz/" />
+          <meta
+            property="og:title"
+            content="집나간토토로의 블로그와 토이 프로젝트"
+          />
+          <meta property="og:image" content="/img/totoro.svg" />
+          <meta
+            property="og:description"
+            content="블로그와 토이프로젝트 사이트"
+          />
+          <meta property="og:type" content="blog" />
+          <meta property="og:site_name" content="Site Name" />
+          <meta property="og:locale" content="en_US" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          {/* 트위터 */}
+          {/* <meta name="twitter:card" content="website" /> */}
+          {/* <meta name="twitter:title" content="집나간토토로의 블로그와 토이 프로젝트" /> */}
+          {/* <meta name="twitter:description" content="블로그와 토이프로젝트 사이트" /> */}
+          {/* <meta name="twitter:image" content="/img/totoro.svg" /> */}
+          {/* iOS */}
+          {/* <meta property="al:ios:url" content="https://blog.ssssksss.xyz/" /> */}
+          {/* <meta property="al:ios:app_store_id" content="ios 앱스토어 ID" />  */}
+          {/* <meta property="al:ios:app_name" content="ios 앱 이름" />  */}
+          {/* Android */}
+          {/* <meta property="al:android:url" content="안드로이드 앱 URL" /> */}
+          {/* <meta property="al:android:app_name" content="안드로이드 앱 이름" /> */}
+          {/* <meta property="al:android:package" content="안드로이드 패키지 이름" />  */}
+          {/* <meta property="al:web:url" content="안드로이드 앱 URL" /> */}
         </Head>
         <body>
           <Main />
