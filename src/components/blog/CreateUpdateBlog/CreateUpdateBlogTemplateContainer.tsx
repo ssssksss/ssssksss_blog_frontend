97a6dd67a511c338c5860e96bdaf7d228fd2b840
SK\ssssk <ssssksss@naver.com>
2024-03-21 03:36:30 +0900

    [style] 외부 스타일 변경으로 인한 스타일 변경

--- src/components/blog/CreateUpdateBlog/CreateUpdateBlogTemplateContainer.tsx
+++ src/components/blog/CreateUpdateBlog/CreateUpdateBlogTemplateContainer.tsx
@@ -11,58 +11,67 @@ import React from 'react';
 const CreateUpdateBlogTemplateContainer = () => {
   return (
     <Container>
-      <ContentTemplateItem
-        onClick={() => {
-          navigator.clipboard.writeText(
-            `| 속성 | 사용 | \n | --- | --- | \n |  |  | \n |  |  | \n |  |  | \n |  |  |`,
-          );
-        }}
-      >
-        테1
-      </ContentTemplateItem>
-      <ContentTemplateItem
-        onClick={() => {
-          navigator.clipboard.writeText(
-            `| 속성 | 사용 | 설명  | \n | --- | --- | --- | \n |  |  |  | \n |  |  |  | \n |  |  |  | \n |  |  |  |`,
-          );
-        }}
-      >
-        테2
-      </ContentTemplateItem>
-      <ContentTemplateItem
-        onClick={() => {
-          const dragText = window.getSelection();
-          navigator.clipboard.readText().then((res) => {
+      <Container1>
+        <ContentTemplateItem
+          onClick={() => {
             navigator.clipboard.writeText(
-              '<a href="' +
-                res +
-                '" target="_blank"> ' +
-                (dragText?.isCollapsed ? res : dragText) +
-                ' </a>',
+              `| 속성 | 사용 | \n | --- | --- | \n |  |  | \n |  |  | \n |  |  | \n |  |  |`,
             );
-            dragText.getRangeAt(0).deleteContents();
-          });
-          store.dispatch(
-            rootActions.toastifyStore.SET_TOASTIFY_MESSAGE({
-              type: 'success',
-              message: '링크로 복사되었습니다.',
-            }),
-          );
-        }}
-      >
-        링크
-      </ContentTemplateItem>
+          }}
+        >
+          테1
+        </ContentTemplateItem>
+        <ContentTemplateItem
+          onClick={() => {
+            navigator.clipboard.writeText(
+              `| 속성 | 사용 | 설명  | \n | --- | --- | --- | \n |  |  |  | \n |  |  |  | \n |  |  |  | \n |  |  |  |`,
+            );
+          }}
+        >
+          테2
+        </ContentTemplateItem>
+        <ContentTemplateItem
+          onClick={() => {
+            const dragText = window.getSelection();
+            navigator.clipboard.readText().then((res) => {
+              navigator.clipboard.writeText(
+                '<a href="' +
+                  res +
+                  '" target="_blank"> ' +
+                  (dragText?.isCollapsed ? res : dragText) +
+                  ' </a>',
+              );
+              dragText.getRangeAt(0).deleteContents();
+            });
+            store.dispatch(
+              rootActions.toastifyStore.SET_TOASTIFY_MESSAGE({
+                type: 'success',
+                message: '링크로 복사되었습니다.',
+              }),
+            );
+          }}
+        >
+          링크
+        </ContentTemplateItem>
+      </Container1>
     </Container>
   );
 };
 export default React.memo(CreateUpdateBlogTemplateContainer);
 
-const Container = styled.section`
-  ${(props) => props.theme.scroll.hidden}
+const Container = styled.div`
   position: sticky;
-  width: max-content;
+  height: 0px;
+  width: 0px;
   left: 100%;
   top: 7.6rem;
+  z-index: 100000;
+`;
+const Container1 = styled.div`
+  ${(props) => props.theme.scroll.hidden}
+  position: absolute;
+  right: 0px;
+  width: max-content;
   display: flex;
   flex-flow: nowrap column;
   background: #eaeaea;
