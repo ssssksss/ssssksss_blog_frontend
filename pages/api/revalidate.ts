f79b03bc7110895f84a2974d0f6d1332d9ce1b58
SK\ssssk <ssssksss@naver.com>
2024-03-20 16:18:23 +0900

    [odr] ODR 코드 수정 후 배포환경에서 테스트

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -4,17 +4,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  // const { query, body } = req;
-  const { body } = req;
+  const { query, body } = req;
 
-  // Check for secret to confirm this is a valid request
-  // if (query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
-  //   return res.status(401).json({ message: 'Invalid token' });
-  // }
+  if (query.secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
 
-  // if (body.path === '') {
-  //   return res.status(401).json({ message: 'Invalid target' });
-  // }
+  if (body.path === '') {
+    return res.status(401).json({ message: 'Invalid target' });
+  }
 
   try {
     await res.revalidate(`/${body.path}/${body.id}`);
--- src/api/BlogAPI.ts
+++ src/api/BlogAPI.ts
@@ -7,6 +7,7 @@ import { store } from '@redux/store';
 import { rootActions } from '@redux/store/actions';
 import { RootState } from '@redux/store/reducers';
 import AxiosInstance from '@utils/axios/AxiosInstance';
+import axios from 'axios';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
@@ -405,13 +406,18 @@ const updateBlog = (props: string) => {
   return useMutationHook({
     mutationFn,
     onSuccessHandler: async ({ variables }) => {
-      await AxiosInstance.post({
-        url:
-          process.env.NEXT_PUBLIC_ABSOLUTE_URL +
-          '/api/revalidate?secret=' +
-          process.env.NEXT_PUBLIC_REVALIDATE_TOKEN,
-        data: { path: 'blog', id: variables.id },
-      })
+      const baseUrl =
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:8080'
+          : 'https://blog-server.ssssksss.xyz';
+      await axios
+        .post({
+          url:
+            baseUrl +
+            '/api/revalidate?secret=' +
+            process.env.NEXT_PUBLIC_REVALIDATE_TOKEN,
+          data: { path: 'blog', id: variables.id },
+        })
         .then((_) => {
           props.onSuccessHandler();
           router.back();
