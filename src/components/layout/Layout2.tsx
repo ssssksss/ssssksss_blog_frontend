fcd56ea0f04cc852c3214d18ec01a1515096950e
SK\ssssk <ssssksss@naver.com>
2024-06-30 00:06:04 +0900

    타입 및 스타일 수정

--- src/components/layout/Layout2.tsx
+++ src/components/layout/Layout2.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import { store } from '@redux/store';
 import { rootActions } from '@redux/store/actions';
+import { RootState } from '@redux/store/reducers';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
@@ -12,7 +13,7 @@ type AppLayoutProps = {
 
 // 블로그 용도의 레이아웃
 const Layout2 = ({ children }: AppLayoutProps) => {
-  const isLoading = useSelector((state) => state.loadingStore.value);
+  const isLoading = useSelector((state: RootState) => state.loadingStore.value);
   const router = useRouter();
   useEffect(() => {
     if (window.localStorage.getItem('theme')) {
@@ -20,7 +21,7 @@ const Layout2 = ({ children }: AppLayoutProps) => {
         rootActions.themeStore.setTheme(window.localStorage.getItem('theme')),
       );
     }
-    const start = (url, { shallow }) => {
+    const start = (url: string, shallow: boolean) => {
       if (shallow) return;
       store.dispatch(rootActions.loadingStore.setIsLoading(true));
     };
@@ -40,8 +41,7 @@ const Layout2 = ({ children }: AppLayoutProps) => {
   }, []);
   return (
     <Container>
-      <ImageBox></ImageBox>
-      <Container1>{children}</Container1>
+      <div className={"max-w-[144rem] px-[.5rem]"}>{children}</div>
       {isLoading && (
         <LoadingComponent w={'100vw'} h={'100vh'} position={'fixed'} />
       )}
@@ -50,33 +50,11 @@ const Layout2 = ({ children }: AppLayoutProps) => {
 };
 
 export default Layout2;
+
 const Container = styled.div`
   border-radius: ${(props) => props.theme.borderRadius.br10};
   position: relative;
   height: 100vh;
   width: 100vw;
   z-index: 2;
-`;
-
-const ImageBox = styled.div`
-  position: fixed;
-  width: 100vw;
-  height: 100vh;
-  z-index: 0;
-`;
-
-const Container1 = styled.main`
-  container-name: main-container;
-  container-type: inline-size;
-  max-width: 144rem;
-  /* LeftBar.tsx에 있는 너비와 맞추어주어야 한다. */
-  /* 마진 0.5rem * 2 + leftBar(4.4rem) */
-  & > div {
-    --top-navbar-height: 4.8rem;
-    max-height: calc(100vh - var(--top-navbar-height));
-    padding: 0.2rem;
-    @media (pointer: coarse) {
-      max-height: calc(100vh - var(--top-navbar-height) - 4.4rem);
-    }
-  }
 `;
