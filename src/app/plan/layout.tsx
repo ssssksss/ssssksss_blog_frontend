0dfab3dd7e697e114c583351332f8ddfc5c3bf8c
SK\ssssk <ssssksss@naver.com>
2025-03-04 02:18:09 +0900

    style: 전체 배경 색상 적용되게 수정

--- src/app/plan/layout.tsx
+++ src/app/plan/layout.tsx
@@ -1,9 +1,9 @@
 import PlanSideBar from "@component/plan/hybrid/PlanSideBar";
-import {ReactNode, Suspense} from "react";
+import { ReactNode, Suspense } from "react";
 
 export default function Layout({children}: {children: ReactNode}) {
   return (
-    <div className="h-full w-full">
+    <div className="h-full w-full bg-default-1">
       <Suspense fallback={<div>Loading...</div>}>
         <PlanSideBar />
       </Suspense>
