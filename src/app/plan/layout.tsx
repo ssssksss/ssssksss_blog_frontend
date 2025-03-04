3d45f7b48d7320a85424d2dc56cba013e9d10bd5
SK\ssssk <ssssksss@naver.com>
2025-03-04 23:36:59 +0900

    style: 페이지 전체 배경색으로 안보이는 문제 수정

--- src/app/plan/layout.tsx
+++ src/app/plan/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode, Suspense } from "react";
 
 export default function Layout({children}: {children: ReactNode}) {
   return (
-    <div className="h-full w-full bg-default-1">
+    <div className="h-auto w-full bg-default-1">
       <Suspense fallback={<div>Loading...</div>}>
         <PlanSideBar />
       </Suspense>
