37d9298d93f782a819c63214873d799f50833117
SK\ssssk <ssssksss@naver.com>
2025-03-09 21:56:17 +0900

    style: 동일한 스타일을 주기 위해 ref 속성 추가

--- src/component/common/input/BasicInput.tsx
+++ src/component/common/input/BasicInput.tsx
@@ -2,13 +2,18 @@ import React from "react";
 
 type BasicInputProps = React.InputHTMLAttributes<HTMLInputElement>;
 
-const BasicInput: React.FC<BasicInputProps> = ({className, ...props}) => {
-  return (
-    <input
-      className={`bg-default-1 primary-outline ${className}`}
-      {...props}
-    />
-  );
-};
+const BasicInput = React.forwardRef<HTMLInputElement, BasicInputProps>(
+  ({className, ...props}, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={`placeholder:text-contrast-1 bg-default-1 primary-outline ${className}`}
+        {...props}
+      />
+    );
+  },
+);
+
+BasicInput.displayName = "BasicInput";
 
 export default BasicInput;
