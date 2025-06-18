422fa4c239e817d968ef131fc72424b36b4fed82
SK\ssssk <ssssksss@naver.com>
2025-06-19 03:52:09 +0900

    chore : url 처리 방식 변경

--- src/app/api/board/route.ts
+++ src/app/api/board/route.ts
@@ -28,7 +28,7 @@ export async function DELETE(request: NextRequest) {
   const url = new URL(request.url);
   const id = url.searchParams.get("id");
   return await fetchApiRoutes({
-    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/board?id=${id}`,
+    url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/board/${id}`,
     req: request,
     isFallbackToErrorPage: false,
   });
