a746e37b2dea23c6bcf6acf7e5bb264875f3656e
SK\ssssk <ssssksss@naver.com>
2025-06-22 00:48:10 +0900

    fix : 조건 에러처리 수정

--- src/utils/api/fetchServerSideInServerComponent.ts
+++ src/utils/api/fetchServerSideInServerComponent.ts
@@ -57,6 +57,7 @@ export const fetchServerSideInServerComponent = async ({
     body: formData ? formData : body ? JSON.stringify(body) : undefined,
     ...(next ? {next} : cache ? {cache} : isAuth ? {cache: "no-store"} : {}),
   });
+  console.log("fetchServerSideInServerComponent.ts 파일 : ",res);
   if (res.status == 401 && _refreshToken && retry > 0 && isAuth) {
     const refreshResponse = await fetch(
       `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user/accessToken`,
@@ -110,7 +111,7 @@ export const fetchServerSideInServerComponent = async ({
       },
     });
   }
-  if (isFallbackToErrorPage) {
+  if (isFallbackToErrorPage && !res.ok) {
     return handleResponseError(res);
   }
   await revalidateTags(handleRevalidateTags);
