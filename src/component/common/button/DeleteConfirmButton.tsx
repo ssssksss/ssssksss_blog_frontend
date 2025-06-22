312a34087f6a1df6cb36b964faead310f7325d8a
SK\ssssk <ssssksss@naver.com>
2025-06-22 16:12:33 +0900

    feat : 삭제 버튼 클릭시 로딩 표시되게 기능 추가

--- src/component/common/button/DeleteConfirmButton.tsx
+++ src/component/common/button/DeleteConfirmButton.tsx
@@ -1,4 +1,6 @@
-import { ButtonHTMLAttributes } from "react";
+"use client";
+
+import { ButtonHTMLAttributes, useState } from "react";
 import { MdDelete } from "react-icons/md";
 import ConfirmModal from "../modal/hybrid/ConfirmModal";
 import ModalButton from "../modal/hybrid/ModalButton";
@@ -13,15 +15,47 @@ interface IDeleteConfirmButton extends ButtonHTMLAttributes<HTMLButtonElement> {
   ariaLabel?: string;
   size?: string;
 }
-const DeleteConfirmButton = (props: IDeleteConfirmButton) => {
+const DeleteConfirmButton = ({
+  onConfirmClick,
+  onCancelClick,
+  className,
+  size,
+  ...props
+}: IDeleteConfirmButton) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleConfirm = async () => {
+    try {
+      setLoading(true);
+      await onConfirmClick(); // Promise 기반 함수라고 가정
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <ModalButton
-      buttonClassName={`relative aspect-square default-flex text-contrast-1 primary-border-radius hover:bg-primary-80 ${props.className || ""}`}
+      buttonClassName={`relative aspect-square default-flex text-contrast-1 disabled:bg-black-40 primary-border-radius hover:bg-primary-80 ${className || ""}`}
       aria-label={props.ariaLabel}
-      modal={<ConfirmModal {...props} />}
+      disabled={loading}
+      modal={
+        <ConfirmModal
+          {...props}
+          loading={loading}
+          onConfirmClick={handleConfirm}
+          onCancelClick={onCancelClick}
+        />
+      }
     >
-      <MdDelete size={`${props.size || "28"}`} />
+      {loading ? (
+        <div className="h-full py-1 default-flex">
+          <div className="border-white-500 aspect-square h-full animate-spin rounded-full border-b-2"></div>
+        </div>
+      ) : (
+        <MdDelete size={`${size || "28"}`} />
+      )}
     </ModalButton>
   );
 };
+
 export default DeleteConfirmButton;
