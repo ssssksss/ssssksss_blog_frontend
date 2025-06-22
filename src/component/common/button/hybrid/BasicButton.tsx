11dbfcb7f3cfc84e39306e16f980de9c98ed3be8
SK\ssssk <ssssksss@naver.com>
2025-06-22 15:53:36 +0900

    feat : 버튼 클릭시 버튼에 로딩 표시 작동 되게 수정

--- src/component/common/button/hybrid/BasicButton.tsx
+++ src/component/common/button/hybrid/BasicButton.tsx
@@ -1,16 +1,46 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 interface IBasicButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
+  showLoadingOnClick?: boolean;
 }
 
 const BasicButton: React.FC<IBasicButton> = ({
   children,
   className,
+  onClick,
+  showLoadingOnClick = false,
   ...props
 }) => {
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  ) => {
+    setLoading(true);
+    try {
+      await onClick!(e);
+    } catch (error) {
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <button className={className} {...props}>
-      {children}
+    <button
+      className={className}
+      onClick={handleClick}
+      disabled={loading || props.disabled}
+      {...props}
+    >
+      {loading ? (
+        <div className="flex h-full py-1 items-center justify-center">
+          <div className="h-full aspect-square animate-spin rounded-full border-b-2 border-white-500"></div>
+        </div>
+      ) : (
+        children
+      )}
     </button>
   );
 };
