2d254ff2172b9c6b6a402d97094044c7626de6d3
SK\ssssk <ssssksss@naver.com>
2025-06-26 16:28:00 +0900

    chore : metadata 추가

--- src/app/workflow/page.tsx
+++ src/app/workflow/page.tsx
@@ -1,7 +1,15 @@
 import WorkflowEditor from "@component/workflow/WorkflowEditor";
 import { firebaseDB } from "@utils/lib/firebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
+import { Metadata } from "next";
 export const revalidate = 0;
+
+export const metadata: Metadata = {
+  title: "워크플로우",
+  description: "워크플로우",
+};
+
+
 const Page = async () => {
   const docRef = doc(firebaseDB, "workflow", "ssssksssBlog");
   const docSnap = await getDoc(docRef);
