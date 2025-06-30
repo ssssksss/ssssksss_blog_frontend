2a04c0d37b40eec235e0b4a6a55d1ced86d3f8c8
SK\ssssk <ssssksss@naver.com>
2025-06-30 21:02:53 +0900

    fix : 블로그 생성시 유효성 검증 변경, 적용중이지는 앟음

--- src/utils/validation/Blog2Yup.ts
+++ src/utils/validation/Blog2Yup.ts
@@ -3,6 +3,12 @@ import * as yup from "yup";
 export const Blog2CreateYup = yup.object().shape({
   firstCategoryId: yup.number().required("필수 입력"),
   secondCategoryId: yup.number().required("필수 입력"),
-  title: yup.string().required("필수 입력"),
-  description: yup.string().required("필수 입력"),
+  title: yup
+    .string()
+    .required("필수 입력")
+    .max(60, "60자 이내로 입력해주세요"),
+  description: yup
+    .string()
+    .required("필수 입력")
+    .max(60, "60자 이내로 입력해주세요"),
 });
