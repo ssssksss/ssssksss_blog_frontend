125d0a28109fbb569a1bd64d6cb54ea5d3955370
SK\ssssk <ssssksss@naver.com>
2025-07-06 02:00:37 +0900

    refactor :  GET 요청이 좋지 않다고 하여 POST로 요청 변경

--- src/app/api/user/accessToken/route.ts
+++ src/app/api/user/accessToken/route.ts
@@ -1,12 +1,14 @@
+
+
 import { cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function POST(request: NextRequest) {
   const refreshToken = cookies().get("refreshToken");
   return await fetch(
     `${process.env.BACKEND_URL}/api/user/accessToken`,
     {
-      method: "GET",
+      method: "POST",
       headers: {
         Cookie: `refreshToken=${refreshToken?.value}`,
       },
